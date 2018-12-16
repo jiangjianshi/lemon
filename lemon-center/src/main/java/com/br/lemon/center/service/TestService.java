4e8311b6b0b1b2ef2e47fd4fbd11a1f39ede3b29
jiangjianshi <jianshi.jiang@100credit.com>
2018-12-16 12:39:49 +0800

    打包

---
+++ lemon-center/src/main/java/com/br/lemon/center/service/TestService.java
@@ -0,0 +1,7 @@
+package com.br.lemon.center.service;
+
+/**
+ * Created by bairong on 2018/12/16.
+ */
+public interface TestService {
+}
---
+++ lemon-center/src/main/java/com/br/lemon/center/service/impl/TestServiceImpl.java
@@ -0,0 +1,9 @@
+package com.br.lemon.center.service.impl;
+
+import com.br.lemon.center.service.TestService;
+
+/**
+ * Created by bairong on 2018/12/16.
+ */
+public class TestServiceImpl implements TestService{
+}
